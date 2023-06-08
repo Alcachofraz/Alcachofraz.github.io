699a2b460cbce7ec4293648b20f07dc96e005518
Pedro Gonçalves <75852333+Alcachofraz@users.noreply.github.com>
2023-06-08 13:43:31 +0100

    .

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -65,10 +65,10 @@ const RESOURCES = {"assets/AssetManifest.json": "5500f4f927c95c9fb93886886af4fd8
 "icons/android-chrome-192x192.png": "9670606808f725b2da9ac1dee45bdefb",
 "icons/android-chrome-512x512.png": "86b1386c26c6f28844a500dd1c5d199e",
 "icons/apple-touch-icon.png": "41b1d6b1fab43bb9ea4a98f789caaa0b",
-"index.html": "5e7c044ade6edd2e391a11f5d33af8d6",
-"/": "5e7c044ade6edd2e391a11f5d33af8d6",
+"index.html": "f8ca43f7b2f724c13c5c95e05914e570",
+"/": "f8ca43f7b2f724c13c5c95e05914e570",
 "logo.png": "b6e7bfa799653d39d66484398fb2e6e2",
-"main.dart.js": "c4ded60292fd382e17cc290a7378971e",
+"main.dart.js": "710df23958e32012c6a768ffa77fd212",
 "manifest.json": "f6b2ceaad7e44edd5e0a0027870ff11f",
 "version.json": "669229fbd408eebd057b1d082b889d95"};
 // The application shell files that are downloaded before a service worker can
